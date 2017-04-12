565240d1b03734e96901c36266a9714c594b3c5c
Nichelle Song <nsong94@uw.edu>
2017-04-11 22:40:35 -0700

    user interaction

--- app/src/main/java/edu/uw/fragmentdemo/MainActivity.java
+++ app/src/main/java/edu/uw/fragmentdemo/MainActivity.java
@@ -1,20 +1,37 @@
 package edu.uw.fragmentdemo;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.PagerAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
+import android.support.v4.view.ViewPager;
 
-public class MainActivity extends AppCompatActivity implements MoviesFragment.OnMovieSelectedListener {
+public class MainActivity extends AppCompatActivity implements MoviesFragment.OnMovieSelectedListener,
+        SearchFragment.OnSearchListener{
 
     private static final String TAG = "MainActivity";
+    private ViewPager viewPager;
+    private PagerAdapter pagerAdapter;
+    private SearchFragment searchFragment;
+    private MoviesFragment moviesFragment;
+    private DetailFragment detailFragment;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        viewPager = (ViewPager) findViewById(R.id.pager);
+        pagerAdapter = new MoviePagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(pagerAdapter);
+
     }
 
 
@@ -31,13 +48,52 @@ public class MainActivity extends AppCompatActivity implements MoviesFragment.On
         ft.commit();
     }
 
+
+    @Override
+    public void onSearchSubmitted(String searchTerm) {
+        moviesFragment = MoviesFragment.newInstance(searchTerm);
+        pagerAdapter.notifyDataSetChanged();
+        viewPager.setCurrentItem(1);
+    }
+
     @Override
     public void onMovieSelected(Movie movie) {
-        DetailFragment detailFragment = DetailFragment.newInstance(movie.toString(), movie.imdbId);
+        detailFragment = DetailFragment.newInstance(movie.toString(), movie.imdbId);
+        pagerAdapter.notifyDataSetChanged();
+        viewPager.setCurrentItem(2);
+    }
 
-        getSupportFragmentManager().beginTransaction()
-                .replace(R.id.container, detailFragment, null)
-                .addToBackStack(null)
-                .commit();
+    private class MoviePagerAdapter extends FragmentStatePagerAdapter {
+
+        public MoviePagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+
+            if (position == 0) {
+                return searchFragment;
+            } else if (position == 1) {
+                return moviesFragment;
+            } else {
+                return detailFragment;
+            }
+        }
+
+        @Override
+        public int getCount() {
+            if (moviesFragment == null) {   // just search
+                return 1;
+            } else if (detailFragment == null) {    // search and movies
+                return 2;
+            } else {    // everything
+                return 3;
+            }
+        }
+
+        public int getItemPosition(Object object) {
+            return POSITION_NONE;
+        }
     }
 }
